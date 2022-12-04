c7e3c182a420a88f03940706d0a7591f1fac68bd
Gabriel Scarano <gabrielscarano2012@hotmail.com>
2022-12-04 17:11:47 -0300

    Finalizando sistema de mensagens

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Moment } from 'src/app/Moment';
 
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -12,7 +14,11 @@ import { MomentService } from 'src/app/services/moment.service';
 export class NewMomentComponent {
   btnText = 'Compartilhar!';
 
-  constructor(private momentService: MomentService) {}
+  constructor(
+    private momentService: MomentService,
+    private messageService: MessagesService,
+    private router: Router,
+  ) {}
 
   async createHandler(moment: Moment) {
     const formData = new FormData();
@@ -24,13 +30,13 @@ export class NewMomentComponent {
       formData.append("image", moment.image);
     }
 
-    // todo
-
     // enviar para o service
     await this.momentService.createMoment(formData).subscribe();
 
     // exibir mensagem
+    this.messageService.add("Momento adicionado com sucesso!");
 
     // redirect
+    this.router.navigate(['/'])
   }
 }
