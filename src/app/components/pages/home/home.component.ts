19d9bfb164fe1729e406556864b5486b287d39e4
Gabriel Scarano <gabrielscarano2012@hotmail.com>
2022-12-04 17:54:51 -0300

    Busca de dados

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -18,7 +18,8 @@ export class HomeComponent implements OnInit {
   moments: Moment[] = []
   baseApiUrl = environment.baseApiUrl
 
-  // todo search
+  faSearch = faSearch;
+  searchTerm: string = '';
 
   constructor(private momentService: MomentService) {}
 
@@ -34,6 +35,15 @@ export class HomeComponent implements OnInit {
       this.moments = data;
 
     })
+  }
+
+  search(e: Event):void {
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
+
+    this.moments = this.allMoments.filter((moment) => {
+      return moment.title.toLowerCase().includes(value.toLowerCase());
+    });
   }
 
 
